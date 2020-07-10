04f0fd5b4a7ee2caeea995fbd8315b23ce4a2314
Tertius Scheepers <Tertiussc@gmail.com>
2020-07-10 10:49:55 +0200

    Completed lesson 94 Search Bar

---
+++ src/components/SearchBar.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { View, TextInput, StyleSheet, } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
+
+
+const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
+    return (
+        <View style={styles.backgroundStyle}>
+            <FontAwesome
+                name="search"
+                style={styles.iconStyle}
+            />
+            <TextInput
+                autoCapitalize="none"
+                autoCorrect={false}
+                placeholder="Search"
+                style={styles.inputStyle}
+                value={term}
+                onChangeText={onTermChange}
+                onEndEditing={onTermSubmit}
+            />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    backgroundStyle: {
+        marginTop: 10,
+        backgroundColor: '#F0EEEE',
+        height: 50,
+        borderRadius: 5,
+        marginHorizontal: 10,
+        flexDirection: 'row'
+    },
+    inputStyle: {
+        fontSize: 18,
+        flex: 1,
+    },
+    iconStyle: {
+        fontSize: 35,
+        alignSelf: 'center',
+        marginHorizontal: 10
+    }
+})
+
+export default SearchBar;
