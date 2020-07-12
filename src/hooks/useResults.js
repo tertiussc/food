23c3a215df39a2dd2ba6289477f71d3c668346c5
Tertius Scheepers <Tertiussc@gmail.com>
2020-07-12 12:02:50 +0200

    completed lesson 104 Flatlist rendered v1

---
+++ src/hooks/useResults.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+import yelp from '../api/yelp'
+
+
+export default () => {
+    const [results, setResults] = useState([])
+    const [errorMessage, setErrorMessage] = useState('')
+
+    const searchApi = async (searchTerm) => {
+        console.log('I ran the Api')
+        try {
+            const response = await yelp.get('/search', {
+                params: {
+                    limit: 50,
+                    term: searchTerm,
+                    location: 'san jose'
+                }
+            })
+            setResults(response.data.businesses)
+        } catch (err) {
+            setErrorMessage('Somthing went wrong')
+        }
+    }
+
+    useEffect(() => {
+        searchApi('pasta')
+    }, [])
+
+    return [searchApi, results, errorMessage]
+}
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,37 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar'
-import yelp from '../api/yelp'
+import useResults from '../hooks/useResults'
+import ResultsList from '../components/ResultsList'
 
 
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('')
-  const [results, setResults] = useState([])
-  const [errorMessage, setErrorMessage] = useState('')
+  const [searhApi, results, errorMessage] = useResults()
 
-  const searchApi = async (searchTerm) => {
-    console.log('I ran the Api')
-    try {
-      const response = await yelp.get('/search', {
-        params: {
-          limit: 50,
-          term: searchTerm,
-          location: 'san jose'
-        }
-      })
-      setResults(response.data.businesses)
-    } catch (err) {
-      setErrorMessage('Somthing went wrong')
-    }
+  const filterResultsByPrice = (price) => {
+    // price === $ || $$ || $$$
+    return results.filter(result => {
+      return result.price === price
+    })
   }
-
-  // call search Api when componentn is first rendered
-  // searchApi('food')
-  useEffect(() => {
-    searchApi('food')
-  }, [])
-
 
   return (
     <View>
@@ -40,8 +24,14 @@ const SearchScreen = () => {
         onTermChange={setTerm}
         onTermSubmit={() => searchApi(term)}
       />
+
       {errorMessage ? <Text>{errorMessage}</Text> : null}
+
       <Text>We have found {results.length} results</Text>
+      <ResultsList results={filterResultsByPrice('$')} title="Cost Effeactive" />
+      <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricier" />
+      <ResultsList results={filterResultsByPrice('$$$')} title="Big Spender" />
+      <ResultsList results={filterResultsByPrice('$$$$')} title="Whale" />
     </View>
   );
 };
